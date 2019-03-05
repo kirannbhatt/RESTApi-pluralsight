438e12791331fcf7f0c0179a46bdbdbd815cc194
Kiran Bhatt <kb_43@outlook.com>
2019-03-05 23:37:57 +0545

    minor change

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-const db = mongoose.connect('mongodb://localhost:27017/pluralsight');
+const db = mongoose.connect('mongodb://localhost:27017/pluralsight', { useNewUrlParser: true });
 
 module.exports = db;
