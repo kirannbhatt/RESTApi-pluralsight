ff02f62587acd8c5bed505b5331d2417fcc15d4d
Kiran Bhatt <kb_43@outlook.com>
2019-03-05 23:37:42 +0545

    bodyparser added

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const router = require('./routes/apiRouter');
 const db = require('./config/database');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use('/', router);
+
 
 app.get('/', (req, res) => {
   res.send('Hello, theres');
